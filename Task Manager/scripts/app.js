44bbe3def72256a3b9cd78790bb7a193b1e10975
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-05-12 23:01:48 -0400

    save May 12th
    needs styling

--- Task Manager/scripts/app.js
+++ Task Manager/scripts/app.js
@@ -4,100 +4,109 @@ var important = false;
 var panelVisible = true;
 var total = 0;
 
-
 function toggleImportance() {
-    if (importaant) {
-        // from imp to not imp
-        $("#iImportant").removeClass(iconImportant).addClass(iconNonImportant);
-        important = false;
-    } else {
-        // non imp to imp
-        $("#iImportant").removeClass(iconNonImportant).addClass(iconImportant);
-        important = true;
-    }
+  if (important) {
+    // from imp to not imp
+    $("#iImportant").removeClass(iconImportant).addClass(iconNonImportant);
+    important = false;
+  } else {
+    // non imp to imp
+    $("#iImportant").removeClass(iconNonImportant).addClass(iconImportant);
+    important = true;
+  }
 }
 
 function togglePanel() {
-    if (panelVisible) {
-        $("#form").hide();
-        $("#btnTogglePanel").text("< Show");
-        panelVisible = false;
-    }else{
-        $("#form").show();
-        $("#btnTogglePanel").text("Hide >");
-        panelVisible = true;
-    }
+  if (panelVisible) {
+    $("#form").hide();
+    $("#btnTogglePanel").text("< Show");
+    panelVisible = false;
+  } else {
+    $("#form").show();
+    $("#btnTogglePanel").text("Hide >");
+    panelVisible = true;
+  }
 }
 
 function saveTask() {
-    let title = $("#txtTitle").val();
-    let desc = $("#txtDesc").val();
-    let dueDate = $("#selDate").val();
-    let location = $("#txtLocation").val();
-    let invites = $("#txtInvites").val();
-    let color = $("#txtColor").val();
-    let frequency = $("#selFrequency").val();
-    let status = $("#SelStatus").val();
+  let title = $("#txtTitle").val();
+  let desc = $("#txtDesc").val();
+  let dueDate = $("#selDate").val();
+  let location = $("#txtLocation").val();
+  let invites = $("#txtInvites").val();
+  let color = $("#txtColor").val();
+  let frequency = $("#selFrequency").val();
+  let status = $("#selStatus").val();
 
-    // create an object
-    let task = new Task(important, title, desc, dueDate, location, invites, color, frequency, status);
-    console.log(task);
-    console.log(JSON.stringify(task));
-    
-    displayTask(task);
-    $.ajax({
-        type: "post",
-        url: "https://fsdiapi.azurewebsites.net/api/tasks/",
-        data: JSON.stringify(task),
-        contentType: "application/json",
-        success: function(res) {
-            console.log("Task saved", res);
-            displayTask(task);
-            clearForm();
-            // update the count
-            total += 1;
-            $("#headCount").text(" You have " + total + " tasks");
-        },
-        error: function(errorDetails) {
-            console.error("Save failed", errorDetails);
-        },
-    });
+  // create an object
+  let task = new Task(
+    important,
+    title,
+    desc,
+    dueDate,
+    location,
+    invites,
+    color,
+    frequency,
+    status,
+  );
+  console.log(task);
+  console.log(JSON.stringify(task));
+
+  displayTask(task);
+  $.ajax({
+    type: "post",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    data: JSON.stringify(task),
+    contentType: "application/json",
+    success: function (res) {
+      console.log("Task saved", res);
+      displayTask(task);
+      clearForm();
+      // update the count
+      total += 1;
+      $("#headCount").text(" You have " + total + " tasks");
+    },
+    error: function (errorDetails) {
+      console.error("Save failed", errorDetails);
+    },
+  });
 }
 
 function clearForm() {
-    $("input").val("");
-    $("textarea").val("");
-    $("select").val("0");
-    $("selColor").val("FFFFFF");
-    important = true;
-    toggleImportance();
+  $("input").val("");
+  $("textarea").val("");
+  $("select").val("0");
+  $("selColor").val("FFFFFF");
+  important = true;
+  toggleImportance();
 }
 
 function getStatusText(status) {
-    switch (status) {
-        case "1":
-            return "Pending";
-        case "2":
-            return "In Progress";
-        case "3":
-            return "Paused";
-        case "4":
-            return "Completed";
-        case "5":
-            return "Abandoned";
+  switch (status) {
+    case "1":
+      return "Pending";
+    case "2":
+      return "In Progress";
+    case "3":
+      return "Paused";
+    case "4":
+      return "Completed";
+    case "5":
+      return "Abandoned";
 
-        default:
-            return "Other";
-    }
+    default:
+      return "Other";
+  }
 }
 
 function displayTask(task) {
-    let iconClass = iconNonImportant
-    if (task.important) {
-        iconClass = iconImportant;
-    }
+  let iconClass = iconNonImportant;
+  if (task.important) {
+    iconClass = iconImportant;
+  }
 
-    let syntax = `<div class="task-item" style="border: 1px solid ${task.color};">
+  let syntax = `<div class="task-item" style="border: 1px solid ${task.color};">
     <div class="icon">
     <i class="${iconClass}"></i>
     </div>
@@ -123,68 +132,67 @@ function displayTask(task) {
 
     </div>`;
 
-    $("#tasks").append(syntax);
+  $("#tasks").append(syntax);
 }
 
 function fetchTasks() {
-    $.ajax({
-        type: "get",
-        url: "https://fsdiapi.azurewebsites.net/api/tasks",
-        success: function (res) {
-            let data = JSON.parse(res); // (decode) from string to obj
-            /**
-             * create a total var
-             * travel the array
-             * if the tasks is yours, increase the total by 1
-             */
-            total = 0;
-            for (let i = 0; i < data.length; i++) {
-                let task = data[i];
-                
-                // if the name attribute of task object is equal to your name,
-                if (task.name == "Arthur") {
-                    total += 1; // total = total + 1;
-                    displayTask(task);
-                }
-            }
+  $.ajax({
+    type: "get",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    success: function (res) {
+      let data = JSON.parse(res); // (decode) from string to obj
+      /**
+       * create a total var
+       * travel the array
+       * if the tasks is yours, increase the total by 1
+       */
+      total = 0;
+      for (let i = 0; i < data.length; i++) {
+        let task = data[i];
 
-            // set the text to the heading
-            $("#headCount").text("You have " + total + " tasks");
-        },
-        error: function(err) {
-            console.error("Error retrieving data", err);
-            },
-    });
-    
+        // if the name attribute of task object is equal to your name,
+        if (task.name == "Arthur") {
+          total += 1; // total = total + 1;
+          displayTask(task);
+        }
+      }
+
+      // set the text to the heading
+      $("#headCount").text("You have " + total + " tasks");
+    },
+    error: function (err) {
+      console.error("Error retrieving data", err);
+    },
+  });
 }
 
 function clearAllTasks() {
-    // ajax DELETE req
-    // /api/tasks/clear/<name>
-    $.ajax({
-        type: "delete",
-        url: "https://fsdiapi.azurewebsites.net/api/tasks",
-        success: function() {
-            //reload the page
-            location.reload();
-        }, 
-        error: function (err) {
-            console.log("Error clearing tasks", err);
-        },
-    });
+  // ajax DELETE req
+  // /api/tasks/clear/<name>
+  $.ajax({
+    type: "delete",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    success: function () {
+      //reload the page
+      location.reload();
+    },
+    error: function (err) {
+      console.log("Error clearing tasks", err);
+    },
+  });
 }
 
 function init() {
-    console.log("Task manager page");
+  console.log("Task manager page");
 
-    // assign events
-    $("#iImportant").click(toggleImportance);
-    $("#btnTogglePanel").click(togglePanel);
-    $("#btnSave").click(saveTask);
-    $("#btnClearAll").click(clearAllTasks);
+  // assign events
+  $("#iImportant").click(toggleImportance);
+  $("#btnTogglePanel").click(togglePanel);
+  $("#btnSaveTask").click(saveTask);
+  $("#btnClearAll").click(clearAllTasks);
 
-    //load data
-    fetchTasks();
+  //load data
+  fetchTasks();
 }
 
 window.onload = init;
